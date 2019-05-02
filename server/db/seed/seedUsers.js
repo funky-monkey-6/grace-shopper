18896b3d9a620c63496df4a0679489c7cd0bfc85
Theo Manton <theo333@gmail.com>
2019-05-01 21:12:37 -0400

    added seed json files, working on seeding db

---
+++ server/db/seed/seedUsers.js
@@ -0,0 +1,32 @@
+module.exports = [
+	{
+		firstName: 'Doug',
+		lastName: 'Funny',
+		email: 'dougf@gmail.com',
+		userType: 'customer'
+	},
+	{
+		firstName: 'Moe',
+		lastName: 'Howard',
+		email: 'moe@stooges.com',
+		userType: 'customer'
+	},
+	{
+		firstName: 'Curly',
+		lastName: 'Howard',
+		email: 'curly@stooges.com',
+		userType: 'customer'
+	},
+	{
+		firstName: 'Larry',
+		lastName: 'Fine',
+		email: 'larry@stooges.com',
+		userType: 'customer'
+	},
+	{
+		firstName: 'Buster',
+		lastName: 'Keaton',
+		email: 'buster@silentfilms.com',
+		userType: 'admin'
+	}
+];
