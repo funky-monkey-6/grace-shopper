079b943feffc3698ad93300f3516f462c96a193e
Justine Morgan <justinemorgan11@gmail.com>
2019-05-03 16:38:06 -0400

    add order + orderItem routes

--- client/store.js
+++ client/store.js
@@ -9,66 +9,66 @@ const SET_PRODUCTS = 'SET_PRODUCTS';
 
 //ACTION CREATORS
 const setUser = user => ({
-    type: SET_USER,
-    user,
+  type: SET_USER,
+  user,
 });
 
 const setProducts = products => ({
-    type: SET_PRODUCTS,
-    products,
+  type: SET_PRODUCTS,
+  products,
 });
 
 //THUNK CREATORS
 export const fetchProducts = () => async dispatch => {
-    try {
-        const response = await axios.get('api/products');
-        const products = response.data;
-        return dispatch(setProducts(products));
-    } catch (error) {
-        throw new Error(error);
-    }
+  try {
+    const response = await axios.get('api/products');
+    const products = response.data;
+    return dispatch(setProducts(products));
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 export const checkUser = enteredUser => async dispatch => {
-    try {
-        const response = await axios.get('api/account');
-        const users = response.data;
-        const user = users.filter(
-            eachUser =>
-                eachUser.email === enteredUser.email && eachUser.password === enteredUser.password,
-        );
-        if (user) {
-            return dispatch(setUser(user));
-        }
-        throw new Error('The email or password you entered is incorrect');
-    } catch (error) {
-        throw new Error(error);
+  try {
+    const response = await axios.get('api/account');
+    const users = response.data;
+    const user = users.filter(
+      eachUser =>
+        eachUser.email === enteredUser.email && eachUser.password === enteredUser.password,
+    );
+    if (user) {
+      return dispatch(setUser(user));
     }
+    throw new Error('The email or password you entered is incorrect');
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 //REDUCERS
 
 const product = (state = {}, action) => {
-    switch (action.type) {
-        case SET_PRODUCTS:
-            return { state: action.products };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return { state: action.products };
+    default:
+      return state;
+  }
 };
 
 const user = (state = {}, action) => {
-    switch (action.type) {
-        case SET_USER:
-            return { ...state, user: action.user };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case SET_USER:
+      return { ...state, user: action.user };
+    default:
+      return state;
+  }
 };
 
 const reducer = combineReducers({
-    product,
-    user,
+  product,
+  user,
 });
 
 export const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunkMiddleware)));
