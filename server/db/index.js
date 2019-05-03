b8cf0a52f7ffd2ece46cecc0986aa85012fd670e
Theo Manton <theo333@gmail.com>
2019-05-02 21:37:03 -0400

    resolving merge conflicts

--- server/db/index.js
+++ server/db/index.js
@@ -9,6 +9,7 @@ const seedUsers = require('../seed/seedUsers');
 // Models:
 
 const Product = conn.define('product', {
+<<<<<<< HEAD
 	// from associations: categoryId
 	title: {
 		type: Sequelize.STRING,
@@ -65,6 +66,64 @@ const Product = conn.define('product', {
 		type: Sequelize.STRING,
 		defaultValue: 'image.png'
 	}
+=======
+  // from associations: categoryId
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: {
+        args: true,
+        msg: 'Title must be provided',
+      },
+    },
+  },
+  variationName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: {
+        args: true,
+        msg: 'Variation Name must be provided',
+      },
+    },
+  },
+  description: {
+    type: Sequelize.TEXT,
+    // allowNull: false,
+    // validate: {
+    // 	notEmpty: {
+    // 		args: true,
+    // 		msg: 'Description must be provided'
+    // 	}
+    // }
+  },
+  inventory: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+    validate: {
+      notEmpty: {
+        args: true,
+        msg: 'Quantity must be provided',
+      },
+    },
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      notEmpty: {
+        args: true,
+        msg: 'Price must be provided',
+      },
+    },
+  },
+  images: {
+    type: Sequelize.STRING,
+    defaultValue: 'image.png',
+  },
+>>>>>>> resolving merge conflicts
 });
 
 const Category = conn.define('category', {
--- server/db/seed/seedUsers.js
+++ server/db/seed/seedUsers.js
@@ -3,35 +3,55 @@ module.exports = [
 		firstName: 'Doug',
 		lastName: 'Funny',
 		email: 'dougf@gmail.com',
+<<<<<<< HEAD
 		userType: 'customer',
 		password: '1234'
+=======
+		userType: 'customer'
+>>>>>>> resolving merge conflicts
 	},
 	{
 		firstName: 'Moe',
 		lastName: 'Howard',
 		email: 'moe@stooges.com',
+<<<<<<< HEAD
 		userType: 'customer',
 		password: '1234'
+=======
+		userType: 'customer'
+>>>>>>> resolving merge conflicts
 	},
 	{
 		firstName: 'Curly',
 		lastName: 'Howard',
 		email: 'curly@stooges.com',
+<<<<<<< HEAD
 		userType: 'customer',
 		password: '1234'
+=======
+		userType: 'customer'
+>>>>>>> resolving merge conflicts
 	},
 	{
 		firstName: 'Larry',
 		lastName: 'Fine',
 		email: 'larry@stooges.com',
+<<<<<<< HEAD
 		userType: 'customer',
 		password: '1234'
+=======
+		userType: 'customer'
+>>>>>>> resolving merge conflicts
 	},
 	{
 		firstName: 'Buster',
 		lastName: 'Keaton',
 		email: 'buster@silentfilms.com',
+<<<<<<< HEAD
 		userType: 'admin',
 		password: '5678'
+=======
+		userType: 'admin'
+>>>>>>> resolving merge conflicts
 	}
 ];
