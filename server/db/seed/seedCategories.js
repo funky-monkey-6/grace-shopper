321f1dcac3474c824ce3e5bcb83b9dc77547af66
Theo Manton <theo333@gmail.com>
2019-05-03 14:36:26 -0400

    fix merge conflicts due to linting

--- server/db/seed/seedCategories.js
+++ server/db/seed/seedCategories.js
@@ -1,5 +1,4 @@
 module.exports = [
-<<<<<<< HEAD
   { name: 'Bibimbap' },
   { name: 'Breakfast Sandwiches' },
   { name: 'Catering' },
@@ -15,21 +14,4 @@ module.exports = [
   { name: 'Smoothies' },
   { name: 'Soup' },
   { name: 'Vegan Tacos' },
-=======
-	{ name: 'Bibimbap' },
-	{ name: 'Breakfast Sandwiches' },
-	{ name: 'Catering' },
-	{ name: 'Coconut Oil Fried Rice' },
-	{ name: 'Coffee & Tea' },
-	{ name: 'Fresh Juice' },
-	{ name: 'Healthy Plates' },
-	{ name: 'Healthy Shots' },
-	{ name: 'Incredibowls' },
-	{ name: 'Juice Cleanse' },
-	{ name: 'Miscellaneous' },
-	{ name: 'Salad' },
-	{ name: 'Smoothies' },
-	{ name: 'Soup' },
-	{ name: 'Vegan Tacos' }
->>>>>>> 18896b3d9a620c63496df4a0679489c7cd0bfc85
 ];
--- server/db/seed/seedUsers.js
+++ server/db/seed/seedUsers.js
@@ -1,5 +1,4 @@
 module.exports = [
-<<<<<<< HEAD
   {
     firstName: 'Doug',
     lastName: 'Funny',
@@ -30,36 +29,4 @@ module.exports = [
     email: 'buster@silentfilms.com',
     userType: 'admin',
   },
-=======
-	{
-		firstName: 'Doug',
-		lastName: 'Funny',
-		email: 'dougf@gmail.com',
-		userType: 'customer'
-	},
-	{
-		firstName: 'Moe',
-		lastName: 'Howard',
-		email: 'moe@stooges.com',
-		userType: 'customer'
-	},
-	{
-		firstName: 'Curly',
-		lastName: 'Howard',
-		email: 'curly@stooges.com',
-		userType: 'customer'
-	},
-	{
-		firstName: 'Larry',
-		lastName: 'Fine',
-		email: 'larry@stooges.com',
-		userType: 'customer'
-	},
-	{
-		firstName: 'Buster',
-		lastName: 'Keaton',
-		email: 'buster@silentfilms.com',
-		userType: 'admin'
-	}
->>>>>>> 18896b3d9a620c63496df4a0679489c7cd0bfc85
 ];
