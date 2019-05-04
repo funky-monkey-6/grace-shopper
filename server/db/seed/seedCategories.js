06e79891b90f399ff389f44c64e4406d5833f634
Theo Manton <theo333@gmail.com>
2019-05-04 10:51:49 -0400

    fix: seeding merge conflicts

---
+++ server/db/seed/seedCategories.js
@@ -0,0 +1,17 @@
+module.exports = [
+	{ name: 'Bibimbap' },
+	{ name: 'Breakfast Sandwiches' },
+	{ name: 'Catering' },
+	{ name: 'Coconut Oil Fried Rice' },
+	{ name: 'Coffee & Tea' },
+	{ name: 'Fresh Juice' },
+	{ name: 'Healthy Plates' },
+	{ name: 'Healthy Shots' },
+	{ name: 'Incredibowls' },
+	{ name: 'Juice Cleanse' },
+	{ name: 'Miscellaneous' },
+	{ name: 'Salad' },
+	{ name: 'Smoothies' },
+	{ name: 'Soup' },
+	{ name: 'Vegan Tacos' }
+];
