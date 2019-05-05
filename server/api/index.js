cef16d3330da3047a9e874bfa5ec8a48bf830a93
Erin McAweeney <ermcaweeney@gmail.com>
2019-05-05 17:05:34 +0100

    fix: merge conflicts

--- server/api/index.js
+++ server/api/index.js
@@ -4,17 +4,10 @@ router.use('/products', require('./products'));
 router.use('/orders', require('./orders'));
 router.use('/users', require('./users'));
 router.use('/categories', require('./categories'));
-<<<<<<< HEAD
-<<<<<<< HEAD
 router.use('/reviews', require('./reviews'));
+router.use('/auth', require('./auth'));
 
 // TO DO - router.use('/cart', require('./cart'));
 // TO DO - router.use('/account', require('./account'));
 
-=======
-router.use('/auth', require('./auth'));
->>>>>>> Sessions set up
-=======
-router.use('/auth', require('./auth'));
->>>>>>> ee587033d55d06221bfcf4441abfea73caa00a72
 module.exports = router;
