0feaceb0162cf9e16f8afeb3fb038f8a618b0308
Justine Morgan <justinemorgan11@gmail.com>
2019-05-06 06:31:07 -0400

    added reviews to ProductSingle

--- client/components/ProductSingle.js
+++ client/components/ProductSingle.js
@@ -1,29 +1,67 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-shadow */
 import React from 'react';
 import { connect } from 'react-redux';
+import { fetchProductReviews, fetchProduct } from '../store';
 
 const mapStateToProps = (state, props) => {
   const { products } = state.product;
-  const product = products.find(prod => prod.id === Number(props.match.params.productId));
+  const { reviews } = state.review;
   return {
-    product,
+    products,
+    reviews,
   };
 };
 
-const ProductSingle = props => {
-  const { product } = props;
-  return (
-    <div>
-      <h1>
-        <i>Placeholder for image</i>
-      </h1>
-      <ul>
-        <li>{product.title}</li>
-        <li>{product.description}</li>
-        <li>{product.price}</li>
-      </ul>
-      <button type="submit">Add to Cart</button>
-    </div>
-  );
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchProductReviews: id => dispatch(fetchProductReviews(id)),
+    fetchProduct: id => dispatch(fetchProduct(id)),
+  };
 };
 
-export default connect(mapStateToProps)(ProductSingle);
+class ProductSingle extends React.Component {
+  componentDidMount = () => {
+    const { productId } = this.props.match.params;
+    const { fetchProduct, fetchProductReviews } = this.props;
+    fetchProductReviews(productId);
+    fetchProduct(productId);
+  };
+
+  render() {
+    const { products, reviews } = this.props;
+    if (!products.length) return null;
+    const product = products[0];
+    return (
+      <div>
+        <h1>
+          <i>Placeholder for image</i>
+        </h1>
+        <ul>
+          <li>{product.title}</li>
+          <li>{product.description}</li>
+          <li>{product.price}</li>
+        </ul>
+        <button type="submit">Add to Cart</button>
+        <div>
+          <h1>
+            <i>Reviews</i>
+          </h1>
+          {reviews.map(review => {
+            return (
+              <ul key={review.id}>
+                <li>{review.rating}</li>
+                <li>{review.comment}</li>
+              </ul>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ProductSingle);
