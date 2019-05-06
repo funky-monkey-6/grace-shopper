eb559dd0112b7a3d5ba1e89f65245e86f97403bf
Justine Morgan <justinemorgan11@gmail.com>
2019-05-06 05:45:50 -0400

    changed data source for Product Single to store

--- client/components/ProductSingle.js
+++ client/components/ProductSingle.js
@@ -1,57 +1,29 @@
-/* eslint-disable react/destructuring-assignment */
 import React from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-class ProductSingle extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      product: {},
-      reviews: [],
-    };
-  }
+const mapStateToProps = (state, props) => {
+  const { products } = state.product;
+  const product = products.find(prod => prod.id === Number(props.match.params.productId));
+  return {
+    product,
+  };
+};
 
-  componentDidMount() {
-    // TODO: what's a more efficient way of doing this (that linter likes)?
-    const { match } = this.props;
-    const { params } = match;
-    const { productId } = params;
+const ProductSingle = props => {
+  const { product } = props;
+  return (
+    <div>
+      <h1>
+        <i>Placeholder for image</i>
+      </h1>
+      <ul>
+        <li>{product.title}</li>
+        <li>{product.description}</li>
+        <li>{product.price}</li>
+      </ul>
+      <button type="submit">Add to Cart</button>
+    </div>
+  );
+};
 
-    axios
-      .get(`api/products/${productId}`)
-      .then(res => res.data)
-      .then(product => this.setState({ product }));
-
-    axios
-      .get(`api/reviews/${productId}`)
-      .then(res => res.data)
-      .then(reviews => this.setState({ reviews }));
-  }
-
-  render() {
-    const { product, reviews } = this.state;
-
-    return (
-      <div>
-        <h1>
-          <i>Placeholder for image</i>
-        </h1>
-        <ul>
-          <li>{product.title}</li>
-          <li>{product.description}</li>
-          <li>{product.price}</li>
-        </ul>
-        <button type="submit">Add to Cart</button>
-        <Link to="/menu">
-          <button type="submit">Return to Main Menu</button>
-        </Link>
-        {reviews.map(review => {
-          return <div key={review.id}>{review.comments}</div>;
-        })}
-      </div>
-    );
-  }
-}
-
-export default ProductSingle;
+export default connect(mapStateToProps)(ProductSingle);
