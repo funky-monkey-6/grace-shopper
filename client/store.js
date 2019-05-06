a27cea8cd068f76d1a197e1ff3d1fe278abe467e
Theo Manton <theo333@gmail.com>
2019-05-06 12:42:38 -0400

    fix: linting issues

--- client/store.js
+++ client/store.js
@@ -184,6 +184,8 @@ const category = (state = { categories: [] }, action) => {
   switch (action.type) {
     case SET_CATEGORIES:
       return { ...state, categories: action.categories };
+    default:
+      return state;
   }
 };
 
