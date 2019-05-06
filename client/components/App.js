af67dea1ae838d7ee18aa3dc1178cdf07ad18f92
Justine Morgan <justinemorgan11@gmail.com>
2019-05-06 06:38:48 -0400

    added more routes to nav

--- client/components/App.js
+++ client/components/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Menu from './Menu';
+import Home from './Home';
+import Login from './Login';
+import Bag from './Bag';
+import Signup from './Signup';
 import ProductSingle from './ProductSingle';
 import Nav from './Nav';
-import Login from './Login';
 
 const App = () => {
   return (
     <Router>
       <div>
         <Nav />
+        <Route exact path="/" component={Home} />
         <Route exact path="/menu" component={Menu} />
-        <Route path="/menu/:productId" component={ProductSingle} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" render={() => <div>Sign Up Here</div>} />
+        <Route exact path="/menu/:productId" component={ProductSingle} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/bag" component={Bag} />
+        <Route exact path="/signup" component={Signup} />
       </div>
     </Router>
   );
