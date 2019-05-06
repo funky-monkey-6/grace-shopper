bbbcf945d41e04043d06082c9791cf1a927f015a
Theo Manton <theo333@gmail.com>
2019-05-06 19:14:16 -0400

    before adding local state

--- client/components/Order.js
+++ client/components/Order.js
@@ -2,63 +2,98 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { fetchOrder, fetchOrderItems } from '../store';
+import OrderItem from './OrderItem';
 
 class Order extends Component {
-  componentDidMount() {
-    // const { fetchOrder, fetchOrderItems } = this.props;
-    this.props
-      .fetchOrder(1)
-      // .then(resp => console.log('order: ', resp));
-      .then(() => {
-        console.log('cart: ', this.props.order.order[0].id);
-        if (this.props.order.order[0]) {
-          this.props.fetchOrderItems(this.props.order.order[0].id);
-        }
-      })
-      .catch(err => console.log(err));
-  }
 
-  render() {
-    const { order, orderItems } = this.props;
-    console.log(order, orderItems);
-    return (
-      <table>
-        <thead>
-          <tr>
-            <th>Product</th>
-            <th>Price</th>
-            <th>Quantity</th>
-            <th>X</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {orderItems.orderItems.map(item => {
-              return <td key={item.id}>{item.productId}</td>;
-            })}
-          </tr>
-        </tbody>
-      </table>
-    );
-  }
+	componentDidMount() {
+		// const { fetchOrder, fetchOrderItems, user, order } = this.props;
+		// TODO grab current logged in user - this.props.user.id
+		this.props.fetchOrder(1)
+			.then(() => {
+				if (this.props.order) {
+					this.props.fetchOrderItems(this.props.order.id);
+				}
+			})
+			.catch(err => console.log(err));
+	}
+
+	render() {
+		const { user, order, orderItems } = this.props;
+		// console.log(order, orderItems);
+		console.log('user: ', user)
+
+
+		let subtotal = 0;
+		if (orderItems) {
+			subtotal = orderItems.reduce((total, item) => total + (item.price * item.quantity), 0);
+		}
+		// TODO save values in db
+		const shipping = order.type === 'delivery' ? 5 : 0;
+
+		const total = subtotal + shipping;
+
+		return (
+			<div>
+				<h2>Bag</h2>
+				<table className='table table-striped table-condensed'>
+					<thead>
+						<tr>
+							<th>Product</th>
+							<th>Price</th>
+							<th>Quantity</th>
+							<th>Item Subtotal</th>
+							<th>Remove</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							orderItems.map(item => {
+								return <OrderItem key={item.id} item={item} />;
+							})
+						}
+						<tr>
+							<td></td>
+							<td></td>
+							<td>
+								{/* TODO drop down - [delivery, pickup] */}
+								Order Type:<br />
+								Subtotal:<br />
+								Shipping:<br />
+								Total:
+							</td>
+							<td>
+								{order.type}<br />
+								{subtotal}<br />
+								{shipping}<br />
+								{total}
+							</td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  const { order, orderItems } = state;
-  return {
-    order,
-    orderItems,
-  };
+	const { user, order, orderItems } = state;
+	return {
+		user,
+		order,
+		orderItems,
+	};
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    fetchOrder: userId => dispatch(fetchOrder(userId)),
-    fetchOrderItems: orderId => dispatch(fetchOrderItems(orderId)),
-  };
+	return {
+		fetchOrder: userId => dispatch(fetchOrder(userId)),
+		fetchOrderItems: orderId => dispatch(fetchOrderItems(orderId)),
+	};
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
+	mapStateToProps,
+	mapDispatchToProps,
 )(Order);
--- client/components/OrderItem.js
+++ client/components/OrderItem.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class OrderItem extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+class OrderItem extends Component {
+	constructor() {
+		super();
+		this.state = {};
+	}
 
-  render() {
-    return <li>item</li>;
-  }
+	render() {
+		const { item, product } = this.props;
+		const { price, quantity } = item;
+		console.log('item: ', item)
+		// console.log('products: ', this.props.products);
+		if (!item.price) {
+			item.price = product.price;
+		}
+
+		const title = product ? product.title : '';
+		const itemTotal = item ? price * quantity : 0;
+		return (
+			<tr>
+				<td>{title}</td>
+				<td>{item.price}</td>
+				<td>{item.quantity}</td>
+				<td>{itemTotal}</td>
+				<td><a href=''>X</a></td>
+			</tr>
+		);
+	};
+	;
 }
+
+const mapStateToProps = (state, { item }) => {
+	const { products } = state;
+	const product = products.find(product => product.id === item.productId)
+	console.log('product: ', product)
+	console.log('state: ', state);
+	return {
+		product
+	}
+};
+
+export default connect(mapStateToProps)(OrderItem);
