06ba4ea1a9a7e1d8091b4d7c6d7ccfed5b68c127
Justine Morgan <justinemorgan11@gmail.com>
2019-05-06 06:38:48 -0400

    added more routes to nav

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import Menu from './Menu';
+import Home from './Home';
+import Login from './Login';
+import Bag from './Bag';
+import Signup from './Signup';
 import ProductSingle from './ProductSingle';
 import Nav from './Nav';
 
@@ -9,8 +13,12 @@ const App = () => {
     <Router>
       <div>
         <Nav />
+        <Route exact path="/" component={Home} />
         <Route exact path="/menu" component={Menu} />
-        <Route path="/menu/:productId" component={ProductSingle} />
+        <Route exact path="/menu/:productId" component={ProductSingle} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/bag" component={Bag} />
+        <Route exact path="/signup" component={Signup} />
       </div>
     </Router>
   );
--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-// TODO: if user is logged in, Login -> Logout
+// TODO: if user is logged in, Login -> Logout and signup disappears
 
 const Nav = () => {
   return (
@@ -15,11 +15,14 @@ const Nav = () => {
         </Link>
       </div>
       <div className="nav-login">
-        <Link to="/" className="nav-item">
+        <Link to="/login" className="nav-item">
           Login
         </Link>
-        <Link to="/" className="nav-item">
-          Cart
+        <Link to="/signup" className="nav-item">
+          Signup
+        </Link>
+        <Link to="/bag" className="nav-item">
+          Bag
         </Link>
       </div>
     </nav>
