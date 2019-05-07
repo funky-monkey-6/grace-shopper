d3fcee2571e4ddc617b5610f090f0556f354b484
Theo Manton <theo333@gmail.com>
2019-05-07 16:15:38 -0400

    feat: add delete and add orderItems

--- client/components/OrderItem.js
+++ client/components/OrderItem.js
@@ -1,45 +1,53 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { deleteOrderItemThunk } from '../store';
+
 class OrderItem extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
+	constructor() {
+		super();
+		this.state = {};
+	}
 
-  render() {
-    const { item, product } = this.props;
-    const { price, quantity } = item;
-    console.log('item: ', item);
-    // console.log('products: ', this.props.products);
-    if (!item.price) {
-      item.price = product.price;
-    }
+	render() {
+		const { orderItem, product, deleteOrderItemThunk, userId, orderId } = this.props;
+		const { price, quantity } = orderItem;
+		console.log('orderItem: ', orderItem);
+		// console.log('products: ', this.props.products);
+		if (!orderItem.price) {
+			orderItem.price = product.price;
+		}
 
-    const title = product ? product.title : '';
-    const itemTotal = item ? price * quantity : 0;
-    return (
-      <tr>
-        <td>{title}</td>
-        <td>{item.price}</td>
-        <td>{item.quantity}</td>
-        <td>{itemTotal}</td>
-        <td>
-          <a href="/#">X</a>
-        </td>
-      </tr>
-    );
-  }
+		const title = product ? product.title : '';
+		const itemTotal = orderItem ? price * quantity : 0;
+		return (
+			<tr>
+				<td>{title}</td>
+				<td>{price}</td>
+				<td>{quantity}</td>
+				<td>{itemTotal}</td>
+				<td>
+					<button onClick={() => deleteOrderItemThunk(userId, orderId, orderItem.id)}>X</button>
+				</td>
+			</tr>
+		);
+	}
 }
 
-const mapStateToProps = (state, { item }) => {
-  const { products } = state;
-  const product = products.find(_product => _product.id === item.productId);
-  console.log('product: ', product);
-  console.log('state: ', state);
-  return {
-    product,
-  };
+const mapStateToProps = (state, { orderItem }) => {
+	const { products } = state;
+	const product = products.find(_product => _product.id === orderItem.productId);
+	console.log('product: ', product);
+	console.log('state: ', state);
+	return {
+		product,
+	};
 };
 
-export default connect(mapStateToProps)(OrderItem);
+const mapDispatchToProps = (dispatch) => {
+	return {
+		deleteOrderItemThunk: (userId, orderId, orderItemId) => dispatch(deleteOrderItemThunk(userId, orderId, orderItemId)),
+	}
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(OrderItem);
