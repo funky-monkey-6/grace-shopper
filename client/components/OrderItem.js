1a63de8ddbdf3b14d0a94cae121f931817ab1695
Theo Manton <theo333@gmail.com>
2019-05-06 22:33:27 -0400

    fix: linting issues

--- client/components/OrderItem.js
+++ client/components/OrderItem.js
@@ -2,44 +2,44 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class OrderItem extends Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-	render() {
-		const { item, product } = this.props;
-		const { price, quantity } = item;
-		console.log('item: ', item);
-		// console.log('products: ', this.props.products);
-		if (!item.price) {
-			item.price = product.price;
-		}
+  render() {
+    const { item, product } = this.props;
+    const { price, quantity } = item;
+    console.log('item: ', item);
+    // console.log('products: ', this.props.products);
+    if (!item.price) {
+      item.price = product.price;
+    }
 
-		const title = product ? product.title : '';
-		const itemTotal = item ? price * quantity : 0;
-		return (
-			<tr>
-				<td>{title}</td>
-				<td>{item.price}</td>
-				<td>{item.quantity}</td>
-				<td>{itemTotal}</td>
-				<td>
-					<a href="/#">X</a>
-				</td>
-			</tr>
-		);
-	}
+    const title = product ? product.title : '';
+    const itemTotal = item ? price * quantity : 0;
+    return (
+      <tr>
+        <td>{title}</td>
+        <td>{item.price}</td>
+        <td>{item.quantity}</td>
+        <td>{itemTotal}</td>
+        <td>
+          <a href="/#">X</a>
+        </td>
+      </tr>
+    );
+  }
 }
 
 const mapStateToProps = (state, { item }) => {
-	const { products } = state;
-	const product = products.find(_product => _product.id === item.productId);
-	console.log('product: ', product);
-	console.log('state: ', state);
-	return {
-		product,
-	};
+  const { products } = state;
+  const product = products.find(_product => _product.id === item.productId);
+  console.log('product: ', product);
+  console.log('state: ', state);
+  return {
+    product,
+  };
 };
 
 export default connect(mapStateToProps)(OrderItem);
