c42bc1e368a7b4fe48cca93e827cbb38fd51b55a
Alex Waite <alexcwaite@gmail.com>
2019-05-07 12:14:27 -0400

    updated App.js with Route for Home comp

--- client/components/App.js
+++ client/components/App.js
@@ -5,6 +5,7 @@ import ProductSingle from './ProductSingle';
 import Nav from './Nav';
 import Login from './Login';
 import Signup from './Signup';
+import Home from './Home';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/menu/:productId" component={ProductSingle} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+        <Route exact path="/" component={Home} />
       </div>
     </Router>
   );
