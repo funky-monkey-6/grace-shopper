90d0a850f23c3e0b4ef9138de049b6b03abba3b2
Theo Manton <theo333@gmail.com>
2019-05-06 22:10:53 -0400

    refactor: updated order put route

--- server/api/users.js
+++ server/api/users.js
@@ -17,7 +17,7 @@ router.post('/adduser/', (req, res, next) => {
 
 // get cart for specific user (if exists)
 router.get('/:userId/cart', (req, res, next) => {
-  Order.findAll({
+  Order.findOne({
     where: {
       userId: req.params.userId,
       status: 'cart',
@@ -86,14 +86,9 @@ router.post('/:userId/orders', (req, res, next) => {
 
 // updating an order itself (not order items) (e.g updating order status)
 router.put('/:userId/orders/:orderId', (req, res, next) => {
-  Order.update(req.body, {
-    returning: true,
-    where: {
-      userId: req.params.userId,
-      id: req.params.orderId,
-    },
-  })
-    .then(([rowsUpdated, [updatedOrder]]) => res.send(updatedOrder))
+  Order.findByPk(req.params.orderId)
+    .then(order => order.update(req.body))
+    .then(updatedOrder => res.send(updatedOrder))
     .catch(next);
 });
 
