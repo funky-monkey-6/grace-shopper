2051a08befd78d604cd54e4cb94e14cab1b9972f
Theo Manton <theo333@gmail.com>
2019-05-09 13:59:50 -0400

    fix: create cart if does not exist when add orderItem to bag

--- client/components/Order.js
+++ client/components/Order.js
@@ -1,147 +1,152 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { fetchOrder as fetchOrderThunk, fetchOrderItems as fetchOrderItemsThunk, updateOrderThunk, fetchProducts } from '../store';
+import {
+  fetchOrder as fetchOrderThunk,
+  fetchOrderItems as fetchOrderItemsThunk,
+  updateOrderThunk,
+  fetchProducts,
+} from '../store';
 import OrderItem from './OrderItem';
 import { isLoggedIn, isCart } from './helperFunctions';
 
 class Order extends Component {
-	componentDidMount() {
-		const { order, fetchOrder, fetchOrderItems, user } = this.props;
-		if (isLoggedIn(user)) {
-			fetchOrder(user.id);
-		}
+  componentDidMount() {
+    const { order, fetchOrder, fetchOrderItems, user } = this.props;
+    if (isLoggedIn(user)) {
+      fetchOrder(user.id);
+    }
 
-		fetchProducts();
-		if (isCart(order)) {
-			console.log('trying to fetch orders...');
-			fetchOrderItems(order.id);
-		}
-	}
+    fetchProducts();
+    if (isCart(order)) {
+      console.log('trying to fetch orders...');
+      fetchOrderItems(order.id);
+    }
+  }
 
-	onChange = ev => {
-		this.props.updateOrderThunk({
-			...this.props.order,
-			type: ev.target.value,
-		});
-	};
+  onChange = ev => {
+    this.props.updateOrderThunk({
+      ...this.props.order,
+      type: ev.target.value,
+    });
+  };
 
-	render() {
-		const { onChange } = this;
-		const { orderItems, order, user, history } = this.props;
+  render() {
+    const { onChange } = this;
+    const { orderItems, order, user, history } = this.props;
 
-		// TODO save values in db for subtotal, shipping, total
-		order.subtotal = 0;
-		if (orderItems) {
-			order.subtotal = orderItems.reduce((total, item) => total + item.price * item.quantity, 0);
-		}
-		order.shipping = order.type === 'delivery' ? 5 : 0;
-		order.total = order.subtotal + order.shipping;
+    // TODO save values in db for subtotal, shipping, total
+    order.subtotal = 0;
+    if (orderItems) {
+      order.subtotal = orderItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    }
+    order.shipping = order.type === 'delivery' ? 5 : 0;
+    order.total = order.subtotal + order.shipping;
 
-		const { subtotal, shipping, total, type } = order;
+    const { subtotal, shipping, total, type } = order;
 
-		console.log({ order });
+    console.log({ order });
 
-		return (
-			<div>
-				<h2>Bag</h2>
-				<table className="table table-striped table-condensed">
-					<thead>
-						<tr>
-							<th>Product</th>
-							<th>Price</th>
-							<th>Quantity</th>
-							<th>Item Subtotal</th>
-							<th>Remove</th>
-						</tr>
-					</thead>
-					<tbody>
-						{isCart(order) && isLoggedIn(user) ? (
-							orderItems.map(orderItem => {
-								return (
-									<OrderItem
-										key={orderItem.id}
-										userId={user.id}
-										orderId={order.id}
-										orderItem={orderItem}
-										history={history}
-									/>
-								);
-							})
-						) : (
-								<tr>
-									<td>Your bag is empty.</td>
-									<td />
-									<td />
-									<td />
-									<td />
-								</tr>
-							)}
-						<tr>
-							<td />
-							<td />
-							<td>
-								{/* TODO drop down - [delivery, pickup] */}
-								Order Type:
+    return (
+      <div>
+        <h2>Bag</h2>
+        <table className="table table-striped table-condensed">
+          <thead>
+            <tr>
+              <th>Product</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Item Subtotal</th>
+              <th>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {isCart(order) && isLoggedIn(user) ? (
+              orderItems.map(orderItem => {
+                return (
+                  <OrderItem
+                    key={orderItem.id}
+                    userId={user.id}
+                    orderId={order.id}
+                    orderItem={orderItem}
+                    history={history}
+                  />
+                );
+              })
+            ) : (
+              <tr>
+                <td>Your bag is empty.</td>
+                <td />
+                <td />
+                <td />
+                <td />
+              </tr>
+            )}
+            <tr>
+              <td />
+              <td />
+              <td>
+                {/* TODO drop down - [delivery, pickup] */}
+                Order Type:
                 <br />
-								Subtotal:
+                Subtotal:
                 <br />
-								Shipping:
+                Shipping:
                 <br />
-								Total:
+                Total:
               </td>
-							<td>
-								<select
-									name="type"
-									value={type}
-									selected="pickup"
-									onChange={onChange}
-									className="form-control"
-								>
-									<option key={1} value="pickup">
-										Pickup
+              <td>
+                <select
+                  name="type"
+                  value={type}
+                  selected="pickup"
+                  onChange={onChange}
+                  className="form-control"
+                >
+                  <option key={1} value="pickup">
+                    Pickup
                   </option>
-									<option key={2} value="delivery">
-										Delivery
+                  <option key={2} value="delivery">
+                    Delivery
                   </option>
-								</select>
-								{type}
-								<br />
-								{subtotal}
-								<br />
-								{shipping}
-								<br />
-								{total}
-							</td>
-							<td />
-						</tr>
-					</tbody>
-				</table>
-				<button type="submit">Start Checkout</button>
-			</div>
-		);
-	}
+                </select>
+                {type}
+                <br />
+                {subtotal}
+                <br />
+                {shipping}
+                <br />
+                {total}
+              </td>
+              <td />
+            </tr>
+          </tbody>
+        </table>
+        <button type="submit">Start Checkout</button>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	const { user, order, orderItems } = state;
-	return {
-		user,
-		order,
-		orderItems,
-	};
+  const { user, order, orderItems } = state;
+  return {
+    user,
+    order,
+    orderItems,
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-	return {
-		fetchOrder: userId => dispatch(fetchOrderThunk(userId)),
-		fetchOrderItems: orderId => dispatch(fetchOrderItemsThunk(orderId)),
-		updateOrderThunk: order => dispatch(updateOrderThunk(order)),
-		fetchProducts: () => dispatch(fetchProducts()),
-	};
+  return {
+    fetchOrder: userId => dispatch(fetchOrderThunk(userId)),
+    fetchOrderItems: orderId => dispatch(fetchOrderItemsThunk(orderId)),
+    updateOrderThunk: order => dispatch(updateOrderThunk(order)),
+    fetchProducts: () => dispatch(fetchProducts()),
+  };
 };
 
 export default connect(
-	mapStateToProps,
-	mapDispatchToProps,
+  mapStateToProps,
+  mapDispatchToProps,
 )(Order);
--- client/components/ProductSingle.js
+++ client/components/ProductSingle.js
@@ -10,6 +10,7 @@ import {
   addOrderThunk,
   addOrderItemThunk,
 } from '../store/index';
+import { isCart } from './helperFunctions';
 
 class ProductSingle extends React.Component {
   componentDidMount = () => {
@@ -19,31 +20,23 @@ class ProductSingle extends React.Component {
     fetchProduct(productId);
   };
 
-  // if adding item
-  // check state.order.status = cart
-  // if not, addOrderThunk()
-  // addOrderItemThunk()
-
   addOrderItem = async (userId, order, orderItem) => {
-    // const { addOrderThunk, addOrderItemThunk } = this.props;
-    // userId = 1; // simulates logged-in user
-    console.log({ userId });
     let newOrder = {};
     try {
-      if (Object.keys(order).length === 0) {
-        console.log('status is cart');
-        // this.props.addOrderThunk(userId, order);
+      if (!isCart(order)) {
         const newOrderObj = {
           type: 'pickup',
           status: 'cart',
+          subtotal: orderItem.price * orderItem.quantity,
+          shipping: 0,
+          total: orderItem.price * orderItem.quantity,
         };
         const newOrderData = await axios.post(`/api/users/${userId}/orders`, newOrderObj);
         newOrder = newOrderData.data;
-        console.log('newOrder: ', newOrder);
       }
-      console.log('adding orderItem');
-      const currOrder = Object.keys(newOrder).length ? newOrder : this.props.order;
-      console.log(currOrder);
+
+      const currOrder = !isCart(order) ? newOrder : this.props.order;
+      console.log({ currOrder })
       await this.props.addOrderItemThunk(userId, currOrder.id, orderItem);
     } catch (err) {
       console.log(err);
