a44d97b21648300cadad638321a373e12067a7d7
Alex Waite <alexcwaite@gmail.com>
2019-05-10 17:11:38 -0400

    updated Checkout component

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,198 +1,278 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchOrder as fetchOrderThunk } from '../store/order';
+import { fetchOrder as fetchOrderThunk, fetchOrderItems } from '../store';
 
 class Checkout extends Component {
-    //   constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //       firstName: '',
-    //       lastName: '',
-    //       email: '',
-    //       shippingAddress: '',
-    //       shippingCity: '',
-    //       shippingState: '',
-    //       billingFirstName: '',
-    //       billingLastName: '',
-    //       billingAddress: '',
-    //       billingCity: '',
-    //       billingState: '',
-    //       billingEmail: '',
-    //       ccNumber: '',
-    //       sameAddressAsAboveStatus: false,
-    //     };
-    //   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      shippingAddress: '',
+      shippingCity: '',
+      shippingState: '',
+      shippingZip: 0,
+      billingFirstName: '',
+      billingLastName: '',
+      billingAddress: '',
+      billingCity: '',
+      billingState: '',
+      billingZip: 0,
+      ccNumber: 0,
+      sameAddressAsAboveStatus: false,
+    };
+  }
 
-    //   componentDidMount = () => {
-    //     const { fetchOrder, user } = this.props;
-    //     fetchOrder(user.id);
-    //   };
+  componentDidMount = () => {
+    const { fetchOrder, user } = this.props;
+    fetchOrder(user.id);
+  };
 
-    //   handleChange = ev => {
-    //     ev.preventDefault();
-    //     this.setState({ [`${ev.target.name}`]: ev.target.value });
-    //   };
+  handleChange = ev => {
+    ev.preventDefault();
+    this.setState({ [`${ev.target.name}`]: ev.target.value });
+  };
 
-    //   handleSubmit = ev => {
-    //     ev.preventDefault();
-    //   };
+  handleSubmit = ev => {
+    ev.preventDefault();
+  };
 
-    //     sameAsAboveButtons = () => {
-    //       const {
-    //         firstName,
-    //         lastName,
-    //         shippingAddress,
-    //         shippingCity,
-    //         shippingState,
-    //         billingFirstName,
-    //         billingLastName,
-    //         billingAddress,
-    //         billingCity,
-    //         billingState,
-    //       } = this.state;
-    //       //add sameAsAboveButton logic, if checked billing information is same as shipping
-    //     };
+  sameAsAboveButtons = () => {
+    const {
+      firstName,
+      lastName,
+      shippingAddress,
+      shippingCity,
+      shippingState,
+      shippingZip,
+      billingFirstName,
+      billingLastName,
+      billingAddress,
+      billingCity,
+      billingState,
+      billingZip,
+    } = this.state;
+    //add sameAsAboveButton logic, if checked billing information is same as shipping
+  };
 
-    //   render() {
-    //     const { handleChange, handleSubmit } = this;
-    //     const {
-    //       firstName,
-    //       lastName,
-    //       shippingAddress,
-    //       shippingCity,
-    //       shippingState,
-    //       email,
-    //       sameAddressAsAboveStatus,
-    //     } = this.state;
-    //     //const { order } = this.state.props;
+  render() {
+    const { handleChange, handleSubmit } = this;
+    const {
+      firstName,
+      lastName,
+      shippingAddress,
+      shippingCity,
+      shippingState,
+      shippingZip,
+      billingFirstName,
+      billingLastName,
+      billingAddress,
+      billingCity,
+      billingState,
+      billingZip,
+      email,
+      sameAddressAsAboveStatus,
+    } = this.state;
 
-    //     return (
-    //       <div>
-    //         <div>
-    //           <h4>Enter checkout information below:</h4>
-    //           <h3>Delivery Information</h3>
-    //           <form onSubmit={handleSubmit}>
-    //             <div className="form-group">
-    //               <label>
-    //                 First Name:
-    //                 <input type="text" name="firstName" value={firstName} onChange={handleChange} />
-    //               </label>
-    //               <label>
-    //                 Last Name:
-    //                 <input type="text" name="lastName" value={lastName} onChange={handleChange} />
-    //               </label>
-    //               <label>
-    //                 Address:
-    //                 <input type="text" name="address" value={shippingAddress} onChange={handleChange} />
-    //               </label>
-    //               <label>
-    //                 City:
-    //                 <input type="text" name="city" value={shippingCity} onChange={handleChange} />
-    //               </label>
-    //               <label>
-    //                 State:
-    //                 <input type="text" name="state" value={shippingState} onChange={handleChange} />
-    //               </label>
-    //               <label>
-    //                 Email:
-    //                 <input type="text" name="email" value={email} onChange={handleChange} />
-    //               </label>
-    //             </div>
+    // const { order } = this.state.props;
+    const { orderItems, order, products } = this.props;
 
-    //             <input type="submit" value="Checkout" />
-    //           </form>
-    //         </div>
-    //         <div>
-    //           <h3>Billing Information</h3>
-    //           <form onSubmit={handleSubmit}>
-    //             <div className="form-group">
-    //               <label>
-    //                 Billing information same as above
-    //                 <input
-    //                   type="check"
-    //                   name="sameAddressAsAboveStatus"
-    //                   checked={sameAddressAsAboveStatus}
-    //                 />
-    //               </label>
-    //               <label>
-    //                 First Name:
-    //                 <input
-    //                   type="text"
-    //                   name="firstName"
-    //                   value={sameAddressAsAboveStatus ? firstName : billingFirstName}
-    //                   onChange={handleChange}
-    //                 />
-    //               </label>
-    //               <label>
-    //                 Last Name:
-    //                 <input
-    //                   type="text"
-    //                   name="lastName"
-    //                   value={sameAddressAsAboveStatus ? lastName : billingLastName}
-    //                   onChange={handleChange}
-    //                 />
-    //               </label>
-    //               <label>
-    //                 Address:
-    //                 <input
-    //                   type="text"
-    //                   name="address"
-    //                   value={sameAddressAsAboveStatus ? shippingAdddress : billingAddress}
-    //                   onChange={handleChange}
-    //                 />
-    //               </label>
-    //               <label>
-    //                 City:
-    //                 <input
-    //                   type="text"
-    //                   name="city"
-    //                   value={sameAddressAsAboveStatus ? shippingCity : billingCity}
-    //                   onChange={handleChange}
-    //                 />
-    //               </label>
-    //               <label>
-    //                 State:
-    //                 <input
-    //                   type="text"
-    //                   name="state"
-    //                   value={sameAddressAsAboveStatus ? shippingState : billingState}
-    //                   onChange={handleChange}
-    //                 />
-    //               </label>
-    //               <label>
-    //                 Email:
-    //                 <input
-    //                   type="text"
-    //                   name="email"
-    //                   value={sameAddressAsAboveStatus ? email : billingEmail}
-    //                   onChange={handleChange}
-    //                 />
-    //               </label>
-    //             </div>
+    // TODO save values in db for subtotal, shipping, total
+    order.subtotal = 0;
+    if (orderItems) {
+      order.subtotal = orderItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    }
+    order.shipping = order.type === 'delivery' ? 5 : 0;
+    order.total = order.subtotal + order.shipping;
 
-    //             <input type="submit" value="Checkout" />
-    //           </form>
-    //         </div>
+    const findProduct = (_products, orderItem) =>
+      _products.find(_product => _product.id === orderItem.productId);
 
-    //         <h4> Review order: </h4>
-    //       </div>
-    //     );
-    //   }
+    return (
+      <div>
+        <div>
+          <h4> Review order: </h4>
+          <table className="table table-condensed">
+            <thead>
+              <tr>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Item Subtotal</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orderItems.map(orderItem => {
+                return (
+                  <tr key={orderItem.id}>
+                    <td>{findProduct(products, orderItem).title}</td>
+                    <td>${orderItem.price}</td>
+                    <td>{orderItem.quantity}</td>
+                    <td>${orderItem ? orderItem.price * orderItem.quantity : 0}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+        <br />
+        <br />
+
+        <div>
+          <h4>Enter checkout information below:</h4>
+          <h3>Delivery Information</h3>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>
+                First Name:
+                <input type="text" name="firstName" value={firstName} onChange={handleChange} />
+              </label>
+              <label>
+                Last Name:
+                <input type="text" name="lastName" value={lastName} onChange={handleChange} />
+              </label>
+              <label>
+                Address:
+                <input
+                  type="text"
+                  name="shippingAddress"
+                  value={shippingAddress}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                City:
+                <input
+                  type="text"
+                  name="shippingCity"
+                  value={shippingCity}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                State:
+                <input
+                  type="text"
+                  name="shippingState"
+                  value={shippingState}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Zip Code:
+                <input type="text" name="shippingZip" value={shippingZip} onChange={handleChange} />
+              </label>
+              <label>
+                Email:
+                <input type="text" name="shippingEmail" value={email} onChange={handleChange} />
+              </label>
+            </div>
+            <br />
+            <br />
+            <div>
+              <h3>Billing Information</h3>
+
+              <label>
+                <input
+                  type="checkBox"
+                  name="sameAddressAsAboveStatus"
+                  checked={sameAddressAsAboveStatus}
+                />
+                Billing information same as above
+              </label>
+              <br />
+              <label>
+                First Name:
+                <input
+                  type="text"
+                  name="billingFirstName"
+                  value={sameAddressAsAboveStatus ? firstName : billingFirstName}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Last Name:
+                <input
+                  type="text"
+                  name="billingLastName"
+                  value={sameAddressAsAboveStatus ? lastName : billingLastName}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Address:
+                <input
+                  type="text"
+                  name="billingAddress"
+                  value={sameAddressAsAboveStatus ? shippingAddress : billingAddress}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                City:
+                <input
+                  type="text"
+                  name="billingCity"
+                  value={sameAddressAsAboveStatus ? shippingCity : billingCity}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                State:
+                <input
+                  type="text"
+                  name="billingState"
+                  value={sameAddressAsAboveStatus ? shippingState : billingState}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Zip Code:
+                <input
+                  type="text"
+                  name="billingZip"
+                  value={sameAddressAsAboveStatus ? shippingZip : billingZip}
+                  onChange={handleChange}
+                />
+              </label>
+              {/* <label>
+                Email:
+                <input
+                  type="text"
+                  name="email"
+                  value={sameAddressAsAboveStatus ? email : billingEmail}
+                  onChange={handleChange}
+                />
+              </label> */}
+            </div>
+
+            <input type="submit" value="Checkout" />
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    const { user, order } = state;
-    return {
-        user,
-        order,
-    };
+  const { user, order, orderItems, products } = state;
+  return {
+    user,
+    order,
+    orderItems,
+    products,
+  };
 };
 
 const mapDispatchToProps = dispatch => {
-    return { fetchOrder: id => dispatch(fetchOrderThunk(id)) };
+  return {
+    fetchOrder: id => dispatch(fetchOrderThunk(id)),
+    fetchOrderItems: orderId => dispatch(fetchOrderItems(orderId)),
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
+  mapStateToProps,
+  mapDispatchToProps,
 )(Checkout);
