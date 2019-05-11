8fd168c27283bed40a1ba1b5439760e00b227f9e
Alex Waite <alexcwaite@gmail.com>
2019-05-10 21:33:59 -0400

    updated with thunk to update order on submit

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
 import { fetchOrder as fetchOrderThunk, fetchOrderItems, updateOrderThunk } from '../store';
 
@@ -21,6 +21,7 @@ class Checkout extends Component {
       billingZip: 0,
       ccNumber: 0,
       sameAddress: false,
+      submitted: false,
     };
   }
 
@@ -37,7 +38,10 @@ class Checkout extends Component {
   };
 
   handleSubmit = ev => {
+    const { updateOrder, order } = this.props;
     ev.preventDefault();
+    updateOrder({ ...order, status: 'processing' });
+    this.setState({ submitted: true });
     // thunk to update user information
     // thunk to update order to status 'processing'
   };
@@ -91,10 +95,11 @@ class Checkout extends Component {
       billingZip,
       email,
       sameAddress,
+      submitted,
     } = this.state;
 
     // const { order } = this.state.props;
-    const { orderItems, order, products } = this.props;
+    const { orderItems, order, products, user } = this.props;
 
     // TODO save values in db for subtotal, shipping, total
     order.subtotal = 0;
@@ -109,163 +114,167 @@ class Checkout extends Component {
 
     return (
       <div>
-        <div>
-          <h4> Review order: </h4>
-          <table className="table table-condensed">
-            <thead>
-              <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Item Subtotal</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orderItems.length
-                ? orderItems.map(orderItem => (
-                    <tr key={orderItem.id}>
-                      <td>{findProduct(products, orderItem).title}</td>
-                      <td>${orderItem.price}</td>
-                      <td>{orderItem.quantity}</td>
-                      <td>${orderItem ? orderItem.price * orderItem.quantity : 0}</td>
-                    </tr>
-                  ))
-                : ''}
-            </tbody>
-          </table>
-        </div>
-        <br />
-
-        <div className="d-flex flex-row">
-          <div>
+        {!submitted ? (
+          <Fragment>
+            <div>
+              <h4> Review order: </h4>
+              <table className="table table-condensed">
+                <thead>
+                  <tr>
+                    <th>Product</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                    <th>Item Subtotal</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {orderItems.length
+                    ? orderItems.map(orderItem => (
+                        <tr key={orderItem.id}>
+                          <td>{findProduct(products, orderItem).title}</td>
+                          <td>${orderItem.price}</td>
+                          <td>{orderItem.quantity}</td>
+                          <td>${orderItem ? orderItem.price * orderItem.quantity : 0}</td>
+                        </tr>
+                      ))
+                    : ''}
+                </tbody>
+              </table>
+            </div>
             <br />
-            Order Type: <span>{order.type}</span>
-            <br />
-            Subtotal: <span>${order.subtotal}</span>
-            <br />
-            Shipping: <span>${order.shipping}</span>
-            <br />
-            <strong>Total: ${order.total}</strong>
-          </div>
-        </div>
-
-        <br />
-        <br />
-
-        <div>
-          <h4>Enter checkout information below:</h4>
-          <h3>Delivery Information</h3>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label>
-                First Name:
-                <input type="text" name="firstName" value={firstName} onChange={handleChange} />
-              </label>
-              <label>
-                Last Name:
-                <input type="text" name="lastName" value={lastName} onChange={handleChange} />
-              </label>
-              <label>
-                Address:
-                <input
-                  type="text"
-                  name="shippingAddress"
-                  value={shippingAddress}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                City:
-                <input
-                  type="text"
-                  name="shippingCity"
-                  value={shippingCity}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                State:
-                <input
-                  type="text"
-                  name="shippingState"
-                  value={shippingState}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                Zip Code:
-                <input type="text" name="shippingZip" value={shippingZip} onChange={handleChange} />
-              </label>
-              <label>
-                Email:
-                <input type="text" name="shippingEmail" value={email} onChange={handleChange} />
-              </label>
+            <div className="d-flex flex-row">
+              <div>
+                <br />
+                Order Type: <span>{order.type}</span>
+                <br />
+                Subtotal: <span>${order.subtotal}</span>
+                <br />
+                Shipping: <span>${order.shipping}</span>
+                <br />
+                <strong>Total: ${order.total}</strong>
+              </div>
             </div>
             <br />
             <br />
             <div>
-              <h3>Billing Information</h3>
+              <h4>Enter checkout information below:</h4>
+              <h3>Delivery Information</h3>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label>
+                    First Name:
+                    <input type="text" name="firstName" value={firstName} onChange={handleChange} />
+                  </label>
+                  <label>
+                    Last Name:
+                    <input type="text" name="lastName" value={lastName} onChange={handleChange} />
+                  </label>
+                  <label>
+                    Address:
+                    <input
+                      type="text"
+                      name="shippingAddress"
+                      value={shippingAddress}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <label>
+                    City:
+                    <input
+                      type="text"
+                      name="shippingCity"
+                      value={shippingCity}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <label>
+                    State:
+                    <input
+                      type="text"
+                      name="shippingState"
+                      value={shippingState}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <label>
+                    Zip Code:
+                    <input
+                      type="text"
+                      name="shippingZip"
+                      value={shippingZip}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <label>
+                    Email:
+                    <input type="text" name="shippingEmail" value={email} onChange={handleChange} />
+                  </label>
+                </div>
+                <br />
+                <br />
+                <div>
+                  <h3>Billing Information</h3>
 
-              <label>
-                <input type="checkBox" name="sameAddress" onChange={this.copyBillingAddress} />
-                Billing information same as above
-              </label>
-              <br />
-              <label>
-                First Name:
-                <input
-                  type="text"
-                  name="billingFirstName"
-                  value={sameAddress ? firstName : billingFirstName}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                Last Name:
-                <input
-                  type="text"
-                  name="billingLastName"
-                  value={sameAddress ? lastName : billingLastName}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                Address:
-                <input
-                  type="text"
-                  name="billingAddress"
-                  value={sameAddress ? shippingAddress : billingAddress}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                City:
-                <input
-                  type="text"
-                  name="billingCity"
-                  value={sameAddress ? shippingCity : billingCity}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                State:
-                <input
-                  type="text"
-                  name="billingState"
-                  value={sameAddress ? shippingState : billingState}
-                  onChange={handleChange}
-                />
-              </label>
-              <label>
-                Zip Code:
-                <input
-                  type="text"
-                  name="billingZip"
-                  value={sameAddress ? shippingZip : billingZip}
-                  onChange={handleChange}
-                />
-              </label>
-              {/* <label>
+                  <label>
+                    <input type="checkBox" name="sameAddress" onChange={this.copyBillingAddress} />
+                    Billing information same as above
+                  </label>
+                  <br />
+                  <label>
+                    First Name:
+                    <input
+                      type="text"
+                      name="billingFirstName"
+                      value={sameAddress ? firstName : billingFirstName}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <label>
+                    Last Name:
+                    <input
+                      type="text"
+                      name="billingLastName"
+                      value={sameAddress ? lastName : billingLastName}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <label>
+                    Address:
+                    <input
+                      type="text"
+                      name="billingAddress"
+                      value={sameAddress ? shippingAddress : billingAddress}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <label>
+                    City:
+                    <input
+                      type="text"
+                      name="billingCity"
+                      value={sameAddress ? shippingCity : billingCity}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <label>
+                    State:
+                    <input
+                      type="text"
+                      name="billingState"
+                      value={sameAddress ? shippingState : billingState}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  <label>
+                    Zip Code:
+                    <input
+                      type="text"
+                      name="billingZip"
+                      value={sameAddress ? shippingZip : billingZip}
+                      onChange={handleChange}
+                    />
+                  </label>
+                  {/* <label>
                 Email:
                 <input
                   type="text"
@@ -274,11 +283,17 @@ class Checkout extends Component {
                   onChange={handleChange}
                 />
               </label> */}
-            </div>
+                </div>
 
-            <input type="submit" value="Checkout" />
-          </form>
-        </div>
+                <button type="submit" onClick={handleSubmit}>
+                  Checkout
+                </button>
+              </form>
+            </div>
+          </Fragment>
+        ) : (
+          <div>Your order has been submitted!</div>
+        )}
       </div>
     );
   }
@@ -298,7 +313,7 @@ const mapDispatchToProps = dispatch => {
   return {
     fetchOrder: id => dispatch(fetchOrderThunk(id)),
     fetchOrderItems: orderId => dispatch(fetchOrderItems(orderId)),
-    updateOrderThunk: order => dispatch(updateOrderThunk(order)),
+    updateOrder: order => dispatch(updateOrderThunk(order)),
   };
 };
 
